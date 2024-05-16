8ab8e9dea172c7938922267f63610a8e2728662b
Viole Hours <holasoyviole@gmail.com>
2024-05-16 19:55:51 -0300

    add(-): ready reset button component

---
+++ Frontend/src/Components/ResetButton.tsx
@@ -0,0 +1,12 @@
+import "./styles/ResetButton.css";
+
+const ResetButton = () => {
+  return (
+    <button className="buttonContainer">
+      <img src="src\assets\reload.svg" alt="Reload" />
+      REINICIAR
+    </button>
+  );
+};
+
+export default ResetButton;
